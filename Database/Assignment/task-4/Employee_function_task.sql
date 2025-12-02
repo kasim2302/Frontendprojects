d18fa29131a0b84237694827371192fee637f4d9
Mohamed Kasim <kasim151000@gmail.com>
2025-12-02 20:09:18 +0530

    task added in db with test

--- Database/Assignment/task-4/Employee_function_task.sql
+++ Database/Assignment/task-4/Employee_function_task.sql
@@ -1,24 +1,47 @@
 use company;
-alter table employee add Joining_date date;
-update employee set Joining_date = '2025-11-08' where id = 1;
+create table employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,        
+    name VARCHAR(50) NOT NULL,          
+    email VARCHAR(100) UNIQUE NOT NULL,       
+    phone VARCHAR(15) UNIQUE,                 
+    salary DECIMAL(10,2) NOT NULL,            
+    joining_date DATE NOT NULL,              
+    department VARCHAR(50),
+    city VARCHAR(50)
+);
+insert into employees (name,email,phone,salary,joining_date,department,city) values
+("Aarav", "aarav.patel@gmail.com", "8709432312", 30000, "2020-03-10", "IT", "Chennai"),     
+("Fatima", "fatima.khan@gmail.com", "9123456780", 45000, "2021-06-15", "HR", "Bangalore"),    
+("Rachel", "rachel.stein@gmail.com", "9234567891", 50000, "2019-09-20", "Finance", "Chennai"), 
+("Joseph", "joseph.williams@gmail.com", "9345678902", 55000, "2022-01-05", "Marketing", "Mumbai"), 
+("Mohammed", "mohammed.ali@gmail.com", "9456789013", 60000, "2020-11-12", "IT", "Chennai"),    
+("Sita", "sita.rao@gmail.com", "9567890124", 65000, "2021-03-22", "HR", "Bangalore"),           
+("David", "david.chen@gmail.com", "9678901235", 70000, "2018-07-30", "Finance", "Mumbai"),   
+("Maria", "maria.garcia@gmail.com", "9789012346", 48000, "2022-08-18", "Marketing", "Chennai"), 
+("Rahul", "rahul.singh@gmail.com", "9890123457", 52000, "2020-05-25", "IT", "Bangalore");
 
-select * from employee;
+select * from employees;
+set sql_safe_updates = 0;
+update employees set name = upper(name);
 
-select department , avg(salary) as avg_dept_salary from employee group by department;
+update employees set name = upper(name);
+update employees set name = replace(name,"a","A");
+update employees set joining_date = "2025-11-15" where id =4;
 
-select * from employee where datediff(curdate(),Joining_date);
-select upper(name) as NAME from employee;
+select department, sum(salary) as total_salary_by_dept from employees group by department;
 
-select city, count(*) as emp_count from employee group by city;
+select * from employees where datediff(curdate(),joining_date) <= 30;
 
-select department, avg(salary) as dept_avg_salary from employee where salary > 50000  group by department;
+select name, round(salary,2) as rounded_salary from employees;
 
-select * from employee where length(name) > 5;
+select * from employees where length(name) > 5;
+select department, avg(salary) as dept_avg_salary from employees group by department having dept_avg_salary >= 50000;
 
-select name,datediff(curdate(),Joining_date) as emp_work_days from employee;
+update employees set salary = round(salary * 1.10,2);
 
-select replace(name,'a','@') as Replaced_names from employee;
+select * from employees;
 
-select name , year(Joining_date) as Joining_year , month(Joining_date) as Joining_month from employee;
+select name ,joining_date,month(joining_date) as Joining_month , year(joining_date) as joining_year from employees order by joining_date
+asc;
 
-select city,max(salary) as high_salary from employee group by city; 
+alter table employees add Bonus int;
